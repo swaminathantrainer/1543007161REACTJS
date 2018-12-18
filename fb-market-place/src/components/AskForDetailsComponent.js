528feabec54a2a8bfa1f0f77737b49c5152e59af
System Administrator <root@MacBook-Pro.local>
2018-12-18 21:16:29 +0530

    Added fb-clone project

---
+++ fb-market-place/src/components/AskForDetailsComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AskForDetailsComponent = () => {
+    return (
+        <div>
+            <button>Ask For Details</button>
+            <p>This automaically sends a message to the seller.</p>
+            <div></div>
+            <img src='/icons/ic_chat.png' />
+            <p>Message</p>
+            <img src='/icons/ic_chat.png' />
+            <p>Save </p>
+            <img src='/icons/ic_chat.png' />
+            <p>Share</p>
+        </div>
+    );
+}
+
+export default AskForDetailsComponent;
